6879138a6feddde7435c4e77c26b561977c7d51a
Manuel Pacheco <manuelalejandropm@gmail.com>
2020-04-05 22:59:43 -0400

    add kotlin configuration

---
+++ src/main/java/com/shadowmanu/terrakova/TerraKovaMod.kt
@@ -0,0 +1,16 @@
+package com.shadowmanu.terrakova;
+
+import com.shadowmanu.terrakova.proxy.CommonProxy
+import net.alexwells.kottle.FMLKotlinModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+
+const val MOD_ID = "terrakova"
+
+@Mod(MOD_ID)
+object TerraKova {
+    private val proxy = CommonProxy()
+
+    init {
+        FMLKotlinModLoadingContext.get().modEventBus.register(proxy)
+    }
+}
---
+++ src/main/java/com/shadowmanu/terrakova/proxy/CommonProxy.kt
@@ -0,0 +1,37 @@
+package com.shadowmanu.terrakova.proxy
+
+import net.minecraft.block.Block
+import net.minecraft.inventory.container.ContainerType
+import net.minecraft.item.Item
+import net.minecraft.tileentity.TileEntityType
+import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import org.apache.logging.log4j.LogManager
+
+public class CommonProxy {
+    private val logger = LogManager.getLogger();
+
+    @SubscribeEvent
+    fun registerBlocks(event: RegistryEvent.Register<Block>) {
+        logger.info("Hello from register block!")
+        // Registry.blocks.forEach { event.registry.register(it) }
+    }
+
+    @SubscribeEvent
+    fun registerTiles(event: RegistryEvent.Register<TileEntityType<*>>) {
+        logger.info("Hello from register tiles!");
+        // Registry.tiles.forEach { event.registry.register(it) }
+    }
+
+    @SubscribeEvent
+    fun registerItems(event: RegistryEvent.Register<Item>) {
+        logger.info("Hello from register items!");
+        // Registry.items.forEach { event.registry.register(it) }
+    }
+
+    @SubscribeEvent
+    fun registerContainers(event: RegistryEvent.Register<ContainerType<*>>) {
+        logger.info("Hello from register containers!");
+        // Registry.containers.forEach { event.registry.register(it) }
+    }
+}
