19be207c2f717ac3ff258163d34f48897e36f6f4
Manuel Pacheco <manuelalejandropm@gmail.com>
2020-04-07 02:19:05 -0400

    use mod id as object member

--- src/main/java/com/shadowmanu/terrakova/TerraKovaMod.kt
+++ src/main/java/com/shadowmanu/terrakova/TerraKovaMod.kt
@@ -4,10 +4,11 @@ import com.shadowmanu.terrakova.proxy.CommonProxy
 import net.alexwells.kottle.FMLKotlinModLoadingContext
 import net.minecraftforge.fml.common.Mod
 
-const val MOD_ID = "terrakova"
-
-@Mod(MOD_ID)
+@Suppress("MemberVisibilityCanBePrivate")
+@Mod(TerraKova.MOD_ID)
 object TerraKova {
+    const val MOD_ID = "terrakova"
+
     private val proxy = CommonProxy()
 
     init {
